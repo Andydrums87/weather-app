786d5c7ecce7a43ae31672223c72585d9d1c708f
Andy Joseph <neon_4@hotmail.com>
2024-08-15 23:05:07 +0100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default ({mode}) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())}
+return  defineConfig({
   plugins: [react()],
-  base: '/weather-app/',
+  define: {
+    'process.env.VIT_WEATHER_API_KEY':JSON.stringify(process.env.VITE_WEATHER_API_KEY)
+  },
+  base: '/translate-app/',
   build: {
     outDir: 'dist',
- }
+ },
 })
+}
+

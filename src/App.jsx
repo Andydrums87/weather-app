2cae6212c2fa9f85e920f37688bf01785b907b65
Andy Joseph <neon_4@hotmail.com>
2024-08-15 22:38:39 +0100

    update key

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const APIKEY = import.meta.env.VITE_WEATHER_API_KEY
   }, [lat, long, units])
 
   const handleData = () => {
-    axios.get(`https://api.openweathermap.org/data/3.0/onecall?units=${units}&lat=${lat}&lon=${long}&appid=${APIKEY}`)
+    axios.get(`https://api.openweathermap.org/data/3.0/onecall?units=${units}&lat=${lat}&lon=${long}&appid=729f65ddf757252c88da2b6644725c22`)
     .then(response => {
       
       setData(response.data);
@@ -53,7 +53,7 @@ const APIKEY = import.meta.env.VITE_WEATHER_API_KEY
   }
 
   const handleSearch = () => {
-    axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=1&appid=${APIKEY}`)
+    axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=1&appid=729f65ddf757252c88da2b6644725c22`)
         .then(response => {
           setLat(response.data[0].lat);
           setLong(response.data[0].lon);
@@ -71,7 +71,7 @@ const APIKEY = import.meta.env.VITE_WEATHER_API_KEY
 
   const handleMajorCities = () => {
 
-    axios.get(`https://api.openweathermap.org/data/2.5/group?id=524901,1273294,2643743&units=${units}&appid=${APIKEY}`)
+    axios.get(`https://api.openweathermap.org/data/2.5/group?id=524901,1273294,2643743&units=${units}&appid=729f65ddf757252c88da2b6644725c22`)
         .then(response => {
           setMajorCities(response.data)
           
@@ -126,18 +126,6 @@ const getIcon = (data) => {
                 return icons[7]
   }
 }
-
-
-
-
-
-
-  
-
-
-
-
-
 
   return (
     <div className="container">

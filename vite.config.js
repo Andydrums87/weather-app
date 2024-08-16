e330f65312bdd921a5878b1169d375980d645f64
Andy Joseph <neon_4@hotmail.com>
2024-08-16 08:51:38 +0100

    updadte workflow

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default ({mode}) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())}
+export default ({mode, command}) => {
+
+  const env = loadEnv(mode, process.cwd(), '')
 return  defineConfig({
   plugins: [react()],
+  define: {
+    __APP_ENV__: JSON.stringify(env.APP_ENV),
+  },
   base: '/weather-app/',
   build: {
     outDir: 'dist',
